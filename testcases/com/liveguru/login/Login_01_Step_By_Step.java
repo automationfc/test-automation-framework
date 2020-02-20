fba23072e5d09c0afaf9ba5edbea6a25050b819a
Automation FC <automationfc.vn@gmail.com>
2020-02-20 16:44:23 +0700

    Create new repo

---
+++ testcases/com/liveguru/login/Login_01_Step_By_Step.java
@@ -0,0 +1,48 @@
+package com.liveguru.login;
+
+import org.testng.annotations.Test;
+import org.testng.annotations.BeforeClass;
+import org.testng.annotations.AfterClass;
+
+public class Login_01_Step_By_Step {
+	@BeforeClass
+	public void beforeClass() {
+
+	}
+	
+	@Test
+	public void TC_01_LoginWithEmptyEmailAndPassword() {
+		
+	}
+	
+	@Test
+	public void TC_02_LoginWithInvalidEmail() {
+		
+	}
+	
+	@Test
+	public void TC_03_LoginWithIncorrectEmail() {
+		
+	}
+	
+	@Test
+	public void TC_04_LoginWithInvalidPassword() {
+		
+	}
+	
+	@Test
+	public void TC_05_LoginWithIncorrectPassword() {
+		
+	}
+
+	@Test
+	public void TC_06_LoginWithValidEmailAndPassword() {
+
+	}
+
+	@AfterClass
+	public void afterClass() {
+
+	}
+
+}
